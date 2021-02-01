510ac85df3694b197aeb7cd4e7385cbfadb81d93
hrebtova.d <hrebtova.d@intervis.ru>
2021-02-01 17:49:31 +0300

    review code

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -29,42 +29,37 @@
     }
 </script>
 
-<style lang="scss">
-    .new-task {
-        width: 768px;
-        min-height: 70px;
-        display: flex;
-        justify-content: space-between;
-        box-sizing: border-box;
-        padding: 20px;
-    }
+<style lang="sass">
+.new-task
+  width: 768px
+  min-height: 70px
+  display: flex
+  justify-content: space-between
+  box-sizing: border-box
+  padding: 20px
 
-    .new-task__text {
-        box-sizing: border-box;
-        width: 560px;
-        min-height: 70px;
-        border: none;
-        background-color: #d1e2f3ab;
-        margin: 0;
-        padding: 10px;
-        box-shadow:  inset 6px 4px -4px rgba(0, 0, 0, .2);
-        border-radius: 10px;
-    }
+.new-task__text
+  box-sizing: border-box
+  width: 560px
+  min-height: 70px
+  border: none
+  background-color: #d1e2f3ab
+  margin: 0
+  padding: 10px
+  box-shadow: inset 6px 4px -4px rgba(0, 0, 0, 0.2)
+  border-radius: 10px
 
-    .new-task__add {
-        width: 150px;
-        border: none;
-        color: white;
-        font-weight: bold;
-        color: #ffffff;
-        font-size: 25px;
-        background-color: #488e87;
-        border-radius: 10px;
-    }
+.new-task__add
+  width: 150px
+  border: none
+  color: white
+  font-weight: bold
+  color: #ffffff
+  font-size: 25px
+  background-color: #488e87
+  border-radius: 10px
 
-    .new-task__add:hover {
-        background-color: #488e87;
-    }
-
+  &:hover
+    background-color: #488e87
 
 </style>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -4,7 +4,7 @@
                @change="$emit('item-completed', todo)" v-model="todo.status === 'done'">
         <label :for="`task${todo.id}`" class="task-container__text">{{todo.text}}</label>
         <button class="task-container__remove" @click="$emit('item-remove', todo)"><img
-                class="task-container__remove-img" src="../assets/delete-icon.svg" alt="delete"></button>
+                class="task-container__remove-img"  alt="delete"></button>
     </div>
 </template>
 
@@ -25,59 +25,53 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-    .task-container {
-        width: 600px;
-        min-height: 50px;
-        background-color: #d1e2f3ab;
-        border-radius: 10px;
-        margin: 15px auto;
-        padding: 10px 20px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        color: #2c3e50;
-        font-size: 20px;
-        font-weight: 700;
-        position: relative;
-    }
+<style scoped lang="sass">
+.task-container
+  width: 600px
+  min-height: 50px
+  background-color: #f2f1fe
+  border-radius: 10px
+  margin: 15px auto
+  padding: 10px 20px
+  display: flex
+  align-items: center
+  justify-content: space-between
+  color: #2c3e50
+  font-size: 20px
+  font-weight: 700
+  position: relative
 
-    .task-container__text {
-        padding-left: 30px;
-        width: 500px;
-        text-align: left;
-    }
+.task-container__text
+  padding-left: 30px
+  width: 500px
+  text-align: left
 
-    .task-container__remove {
-        height: 20px;
-        width: 20px;
-        background-color: transparent;
-        border: none;
-        padding: 0;
-    }
+.task-container__remove
+  height: 20px
+  width: 20px
+  background-color: transparent
+  border: none
+  padding: 0
 
-    .task-container__checkbox {
-        display: none;
-        position: relative;
-    }
+.task-container__checkbox
+  display: none
+  position: relative
 
-    .task-container__checkbox + label::before {
-        position: absolute;
-        content: "";
-        top: 25px;
-        left: 15px;
-        width: 20px;
-        height: 20px;
-        background-color: #2c3e50;
-    }
+  + label::before
+    position: absolute
+    content: ""
+    top: 25px
+    left: 15px
+    width: 20px
+    height: 20px
+    background-color: #2c3e50
 
-    .task-container__checkbox:checked + label::after {
-        position: absolute;
-        content: "";
-        top: 30px;
-        left: 20px;
-        width: 10px;
-        height: 10px;
-        background-color: #d1e2f3ab;
-    }
+  &:checked + label::after
+    position: absolute
+    content: ""
+    top: 30px
+    left: 20px
+    width: 10px
+    height: 10px
+    background-color: #d1e2f3ab
 </style>
