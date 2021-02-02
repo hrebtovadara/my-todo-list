1cb96072b8d88a53d286fde1eafef62f42a44685
dasha <hrebtovadara@gmail.com>
2021-02-02 20:40:32 +0300

    add state data

--- src/store/index.js
+++ src/store/index.js
@@ -6,14 +6,34 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     data: {
+      countBoards: 2,
+      countTasks: 4,
       boards: [
         {
           id: 1,
           name: 'Досочка 1',
-          icon: '',
-          link: '/tasknumone',
-          tasks: []
+          icon: ''
         }
+      ],
+      tasks: [
+        {
+          id: 1,
+          boardId: 1,
+          text: 'погладить котика',
+          status: ' ',
+        },
+        {
+          id: 2,
+          boardId: 1,
+          text: 'погладить котика',
+          status: ' ',
+        },
+        {
+          id: 3,
+          boardId: 1,
+          text: 'погладить котика',
+          status: ' ',
+        },
       ]
     }
   },
