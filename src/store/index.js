66eb4edacfdf2d7360edeac987b9886ccc499830
hrebtovadara <hrebtovadara@gmail.com>
2021-02-09 12:12:02 +0300

    add store.state in local.storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+
 
 
 Vue.use(Vuex)
@@ -107,6 +109,7 @@ export default new Vuex.Store({
   },
   modules: {
   },
+  plugins: [createPersistedState()],
   strict: process.env.NODE_ENV !== 'production'
 })
 
